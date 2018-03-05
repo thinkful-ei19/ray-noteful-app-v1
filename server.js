8195cbd659e37578c18a8602e65c1d5875329764
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-03-05 14:08:31 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 'use strict';
 
 // TEMP: Simple In-Memory Database
+const express = require('express');
+
 const data = require('./db/notes');
+
+const app = express();
+app.use(express.static('public'));
+
+//Listen for incoming connections
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+
 
 console.log('hello world!');
 
