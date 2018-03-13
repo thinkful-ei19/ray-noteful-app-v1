c4e4e826267822f38044d1518be5a73a082ecffb
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-03-12 22:35:57 -0700

    added SQL components

---
+++ notes.queries.sql
@@ -0,0 +1,16 @@
+-- SELECT * from notes;
+
+-- SELECT * from notes LIMIT 5;
+
+-- SELECT * from notes ORDER BY id DESC;
+-- SELECT * from notes ORDER BY title ASC LIMIT 7;
+-- SELECT * from notes ORDER BY created ASC LIMIT 5;
+
+-- SELECT id, title from notes WHERE title = '11 ways investing in cats can make you a millionaire';
+
+-- SELECT title from notes WHERE title LIKE '%gaga%';
+
+-- UPDATE notes SET title = 'Updated Title' WHERE title = '5 life lessons learned from cats'
+
+-- DELETE from notes WHERE id = 1007;
+
